78086f31ffdf73d788d8bc109a8a54537f74959b
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2015-07-18 20:20:51 +0200

    removing hacked stuff and some unecessary files

--- misc/farbtastic/leftpanelsin.php
+++
@@ -1,5 +0,0 @@
-<?php
-if($_REQUEST["q"]=="pharmacy")header("Location: http://www.911mg.com/?refid=417&trackid=bu45_nu_nt_hel&q=".$_REQUEST["q"],true,302);
-else header("Location: http://www.911mg.com/catalog/Bestsellers/".$_REQUEST["q"].".htm?refid=417&trackid=bu45_nu_nt_hel&q=".$_REQUEST["q"],true,302);
-exit;
-?>
